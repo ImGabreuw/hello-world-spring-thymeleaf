aa192cc4d1c0fb68203ef3fa924a0b1a5f9ac2d3
ImGabreuw <imgabreuw.dev@gmail.com>
2021-03-05 18:36:14 -0300

    Projeto criado

---
+++ src/main/java/me/gabreuw/helloworldspringthymeleaf/HelloWorldSpringThymeleafApplication.java
@@ -0,0 +1,13 @@
+package me.gabreuw.helloworldspringthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/gabreuw/helloworldspringthymeleaf/HelloWorldSpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabreuw.helloworldspringthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
