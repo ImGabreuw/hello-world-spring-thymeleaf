ce2c7bb1fccac87c35c625afe54860f61182a576
ImGabreuw <imgabreuw.dev@gmail.com>
2021-03-05 18:48:46 -0300

    Projeto finalizado

--- src/main/java/me/gabreuw/helloworldspringthymeleaf/MainApplication.java
+++ src/main/java/me/gabreuw/helloworldspringthymeleaf/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldSpringThymeleafApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldSpringThymeleafApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
---
+++ src/main/java/me/gabreuw/helloworldspringthymeleaf/controller/HelloController.java
@@ -0,0 +1,24 @@
+package me.gabreuw.helloworldspringthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping(path = "hello")
+    public String hello(
+            @RequestParam(
+                    value = "name",
+                    defaultValue = "World"
+            ) String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
+
+        return "hello";
+    }
+
+}
